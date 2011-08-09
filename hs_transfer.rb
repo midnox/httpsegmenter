24689655d612da7fc29c86a536cea3c7d2f9614c
ePirat <info@epirat.de>
2011-08-10 02:03:07 +0300

    Added multiple ftp server upload, for example see the example-conf/config-simple.yml

--- hs_transfer.rb
+++ hs_transfer.rb
@@ -16,6 +16,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
+# Added multiple ftp server support! But still can cause error 'cause i am not checking anything...
+
 require 'rubygems'
 
 class HSTransfer
